cdb4f532d20b7f4df6ef5a8d05b4b0b06b413787
Yoel Ribero <yoelribero9@gmail.com>
2020-03-23 22:38:39 -0300

    Agrega clases a lógica de proyecto

--- 6-Proyecto_generador_nombres_restApi_ajax/js/app_class.js
+++ 6-Proyecto_generador_nombres_restApi_ajax/js/app_class.js
@@ -1,6 +1,6 @@
 class API {
-    async getNames() {
-        const data = await fetch('https://uinames.com/api/');
+    async getNames(url) {
+        const data = await fetch(url);
         const dataResult = await data.json();
         return dataResult;
     }
@@ -8,18 +8,49 @@ class API {
 
 class Interfaz {
     
-    async namesList() {
-        const names = new API;
-        const namesList = await names.getNames();
-        debugger;
-        console.log(namesList); 
+    namesList(data) {
+        let html = '<h2>Nombres Generados</h2>'
+        html += `<ul class="lista">`
+        data.forEach(nombre => {
+            html += `
+                <li>${nombre.name}</li>
+            `
+        });
+        html += `</ul>`
+        document.querySelector('#resultado').innerHTML = html;
     } 
 }
 
 document.querySelector('#generar-nombre').addEventListener('submit', cargarNombres);
 
-function cargarNombres(e) {
+async function cargarNombres(e) {
     e.preventDefault();
+    // Leer variables
+    const origen = document.getElementById('origen');
+    const origenSeleccionado = origen.options[origen.selectedIndex].value;
+    const genero = document.getElementById('genero');
+    const generoSeleccionado = genero.options[genero.selectedIndex].value;
+    const cantidad = document.getElementById('numero').value;
+
+    let url = '';
+    url += 'https://uinames.com/api/?';
+    // Si hay orígen agregarlo a la url
+    if (origenSeleccionado !== '') {
+        url += `region=${origenSeleccionado}&`;
+    }
+    // Si hay género agregarlo a la url
+    if (generoSeleccionado !== '') {
+        url += `gender=${generoSeleccionado}&`;
+    }
+    // Si hay una cantidad agregarlo a la url
+    if (cantidad !== '') {
+        url += `amount=${cantidad}&`;
+    }
+
+    // Instancio clase de fetch y obtengo datos
+    const rest = new API();
+    const data = await rest.getNames(url);
+    // Pinto datos en pantalla
     const ui = new Interfaz();
-    ui.namesList();
+    ui.namesList(data);
 }
